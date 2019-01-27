fd6566f2d28e766bb1d9c7b576e53fd9f6b64969
Parker Drouillard <Parker@pepcorp.ca>
2019-01-27 00:36:04 -0500

    added learning page

--- learning_modules/urls.py
+++ learning_modules/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 
-app_name = 'orders'
+app_name = 'learning_modules'
 
 urlpatterns = [
     path('', views.base, name = 'base'),
-
+    path('learn/', views.learn, name = 'learn'),
 ]
--- learning_modules/views.py
+++ learning_modules/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def base(request):
     return render(request, 'learning_modules/base.html')
+
+def learn(request):
+    return render(request, 'learning_modules/learn.html')

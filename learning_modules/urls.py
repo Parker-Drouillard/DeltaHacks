4d246b47dc8ef140063520e91813e4cb00f379ff
Parker Drouillard <Parker@pepcorp.ca>
2019-01-27 00:49:28 -0500

    Removed additional conflicts in urls.py

--- learning_modules/urls.py
+++ learning_modules/urls.py
@@ -5,13 +5,10 @@ from . import views
 app_name = 'learning_modules'
 
 urlpatterns = [
-<<<<<<< HEAD
+
     path('', views.index, name = 'index'),
     path('learning/', views.learning, name = 'learning')
-=======
-    path('', views.base, name = 'base'),
-    path('learn/', views.learn, name = 'learn'),
     path('compare/', views.compare, name = 'compare'),
     path('connect/', views.connect, name = 'connect'),
->>>>>>> perfectView
+
 ]

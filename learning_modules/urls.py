ee4088efdd46561674a3fed17b322f08a76aef84
Mohamad <bazzimoe3@gmail.com>
2019-01-27 03:03:36 -0500

    fixed base.html extensions

--- learning_modules/urls.py
+++ learning_modules/urls.py
@@ -6,7 +6,7 @@ app_name = 'learning_modules'
 
 urlpatterns = [
 
-    path('', views.index, name = 'index'),
+    path('index/', views.index, name = 'index'),
     path('learning/', views.learning, name = 'learning'),
     path('compare/', views.compare, name = 'compare'),
     path('compare-info/', views.add_compare_info, name='add_compare_info')
--- learning_modules/views.py
+++ learning_modules/views.py
@@ -3,14 +3,12 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'learning_modules/index.html')
 
-
 def learning(request):
     return render(request, 'learning_modules/learning.html')
-
 
 def compare(request):
     return render(request, 'learning_modules/compare.html')
 
-
 def add_compare_info(request):
     tickers = []
+    pass

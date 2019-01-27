9940ab3a531e3272339a0aa8eb1177d5d6fe6ce1
Parker Drouillard <Parker@pepcorp.ca>
2019-01-26 23:06:52 -0500

    added some files

---
+++ learning_modules/static/JS/main.js
@@ -0,0 +1,12 @@
+window.onscroll = function (e) {
+    var vertical_position = 0;
+    if (pageYOffset)//usual
+      vertical_position = pageYOffset;
+    else if (document.documentElement.clientHeight)//ie
+      vertical_position = document.documentElement.scrollTop;
+    else if (document.body)//ie quirks
+      vertical_position = document.body.scrollTop;
+  
+    var your_div = document.getElementById('whiteout');
+    your_div.top = (vertical_position + 200) + 'px';//200 is arbitrary.. just to show you could now position it how you want
+  }
